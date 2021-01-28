e96b193f0111d14e1cfbf5e8a7792297a86f86ab
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-01-28 13:29:21 +0000

    glslang

---
+++ pkgs/glslang/build.sh
@@ -0,0 +1,31 @@
+pkgname=glslang
+pkgver=11.1.0
+
+fetch() {
+	curl -L "https://github.com/KhronosGroup/glslang/archive/11.1.0.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	cmake -G Ninja ../ \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX=/usr \
+		-DCMAKE_INSTALL_LIBDIR=lib \
+		-DBUILD_SHARED_LIBS=ON
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE.txt
+#	cat COPYING
+}
