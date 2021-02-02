754ee5d0a97c226d7c5afa58a558483228059f77
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-02-02 13:35:17 +0000

    Added rsync

---
+++ pkgs/rsync/build.sh
@@ -0,0 +1,30 @@
+pkgname=rsync
+pkgver=3.2.3
+
+fetch() {
+	curl "https://download.samba.org/pub/rsync/src/rsync-3.2.3.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+}
+
+build() {
+	cd $pkgname-$pkgver
+	./configure \
+		--prefix=/usr \
+		--build=x86_64-unknown-linux-musl \
+		--host=x86_64-unknown-linux-musl \
+                --disable-xxhash \
+                --disable-zstd \
+                --disable-lz4
+
+	make
+}
+
+package() {
+	cd $pkgname-$pkgver
+	make install DESTDIR=$pkgdir
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING
+}
