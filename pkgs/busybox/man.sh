3dc6a96584487185958f23be5d7bf70de0a97cf4
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-02-06 12:18:46 +0000

    Improve default man colours

--- pkgs/busybox/man.sh
+++ pkgs/busybox/man.sh
@@ -1,1 +1,8 @@
+export LESS_TERMCAP_md=$'\e[01;34m' \
+export LESS_TERMCAP_me=$'\e[0m' \
+export LESS_TERMCAP_se=$'\e[0m' \
+export LESS_TERMCAP_so=$'\e[01;44;35m' \
+export LESS_TERMCAP_ue=$'\e[0m' \
+export LESS_TERMCAP_us=$'\e[01;33m' \
+
 export MANPAGER="less -r"
