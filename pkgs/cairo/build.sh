f772cfd434ea38d12645e71547d89d6e07d7ad52
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-02-23 16:17:40 +0000

    Added cairo (glib disabled for now)

---
+++ pkgs/cairo/build.sh
@@ -0,0 +1,30 @@
+pkgname=cairo
+pkgver=master
+
+fetch() {
+	curl "https://gitlab.freedesktop.org/cairo/cairo/-/archive/master/cairo-master.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib \
+		-Dglib=disabled
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING*
+}
