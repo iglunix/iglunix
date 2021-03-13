3d3a5750321b2306df7eeeb99819d60bdf0685ab
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-03-13 22:24:47 +0000

    added package kak-lsp

---
+++ pkgs/kak-lsp/build.sh
@@ -0,0 +1,24 @@
+pkgname=kak-lsp
+pkgver=9.0.0
+
+fetch() {
+	curl -L "https://github.com/kak-lsp/kak-lsp/archive/v$pkgver.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cargo build --release --locked --all-features
+}
+
+package() {
+	cd $pkgname-$pkgver
+	install -Dm755 target/release/$pkgname $pkgdir/usr/bin/
+	install -Dm644 rc/lsp.kak $pkgdir/usr/share/kak-lsp/rc/
+	install -Dm644 kak-lsp.toml $pkgdir/usr/share/kak-lsp/examples/
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat UNLICENSE
+}
