375ad77edbae315c1d1b1302e6d6a8c1853562a7
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-03-16 18:27:36 +0000

    Added libpsl

---
+++ pkgs/libpsl/build.sh
@@ -0,0 +1,30 @@
+pkgname=libpsl
+pkgver=0.21.1
+
+fetch() {
+	curl -L "https://github.com/rockdaboot/libpsl/releases/download/0.21.1/libpsl-0.21.1.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
