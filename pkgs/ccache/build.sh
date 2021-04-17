19e818dae97295bbd7a3394ba3bce9883fbc6028
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-04-17 14:13:04 +0100

    Added ccache

---
+++ pkgs/ccache/build.sh
@@ -0,0 +1,30 @@
+pkgname=ccache
+pkgver=4.2.1
+
+fetch() {
+	curl -L "https://github.com/ccache/ccache/releases/download/v4.2.1/ccache-4.2.1.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	cmake -G Ninja ../ \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX=/usr \
+		-DCMAKE_INSTALL_LIBDIR=lib
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
