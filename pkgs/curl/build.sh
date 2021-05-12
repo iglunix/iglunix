b55500afa1628fd7e910c10ad6e50be35d44a746
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-05-12 09:58:24 +0100

    curl: set path to ca bundle and update

--- pkgs/curl/build.sh
+++ pkgs/curl/build.sh
@@ -1,11 +1,12 @@
-pkgver=7.75.0
+pkgver=7.76.1
+pkg_ver=$(echo $pkgver | tr '.' '_')
 pkgname=curl
 pkgrel=1
 bad=""
 ext="doc:dev"
 
 fetch() {
-	curl -L "https://github.com/curl/curl/releases/download/curl-7_75_0/curl-7.75.0.tar.xz" -o $pkgname-$pkgver.tar.gz
+	curl -L "https://github.com/curl/curl/releases/download/curl-$pkg_ver/curl-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.gz
 	tar -xf $pkgname-$pkgver.tar.gz
 }
 
@@ -16,7 +17,8 @@ build() {
 	cmake -G Ninja ../ \
 		-DCMAKE_BUILD_TYPE=Release \
 		-DCMAKE_INSTALL_PREFIX=/usr \
-		-DCMAKE_INSTALL_LIBDIR=lib
+		-DCMAKE_INSTALL_LIBDIR=lib \
+		-DCURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 	samu
 }
 
@@ -38,7 +40,6 @@ package_dev() {
 	rm -r $pkgdir/usr/bin
 	rm $pkgdir/usr/lib/*.so
 	rm $pkgdir/usr/lib/*.so.*
-	env
 }
 
 license() {
