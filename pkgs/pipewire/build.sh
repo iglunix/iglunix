958e3f04ec949b827bb1c9a66a298f99487b8e77
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-05-16 15:13:21 +0100

    Added pipewire

---
+++ pkgs/pipewire/build.sh
@@ -0,0 +1,41 @@
+pkgname=pipewire
+pkgver=0.3.27
+
+fetch() {
+	curl "https://gitlab.freedesktop.org/pipewire/pipewire/-/archive/$pkgver/pipewire-$pkgver.tar.bz2" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+
+	cp ../no-dbus.patch .
+	cd $pkgname-$pkgver
+	patch -p1 < ../no-dbus.patch
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib \
+		-Dbluez5=disabled \
+		-Dspa-plugins=disabled \
+		-Dlibcamera=disabled \
+		-Dexamples=disabled \
+		-Dgstreamer=disabled \
+		-Dlibpulse=disabled
+
+
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING
+}
