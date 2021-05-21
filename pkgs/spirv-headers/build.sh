cbbf032021bd71e14cb7175ec161ec720bb45d35
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-05-21 10:44:46 +0100

    added spirv-headers

---
+++ pkgs/spirv-headers/build.sh
@@ -0,0 +1,31 @@
+pkgname=spirv-headers
+pkgver=master
+
+fetch() {
+	curl -L "https://github.com/KhronosGroup/SPIRV-Headers/archive/refs/heads/master.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mv SPIRV-Headers-$pkgver spirv-headers-$pkgver
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	cmake -G Ninja ../ \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX=/usr \
+		-DCMAKE_INSTALL_LIBDIR=lib
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
