25e86ce9d6a5c23d278260c661aa3c03a28edd81
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-05-21 10:46:25 +0100

    added lynx

---
+++ pkgs/lynx/build.sh
@@ -0,0 +1,31 @@
+pkgname=lynx
+pkgver=2.8.9
+
+fetch() {
+	curl "https://invisible-mirror.net/archives/lynx/tarballs/lynx2.8.9rel.1.tar.bz2" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mv lynx2.8.9rel.1 $pkgname-$pkgver
+}
+
+build() {
+	cd $pkgname-$pkgver
+	./configure \
+		--prefix=/usr \
+		--sysconfdir=/etc \
+		--with-ssl=/usr \
+		--build=x86_64-unknown-linux-musl \
+		--host=x86_64-unknown-linux-musl
+
+	gmake
+}
+
+package() {
+	cd $pkgname-$pkgver
+	gmake install DESTDIR=$pkgdir
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
