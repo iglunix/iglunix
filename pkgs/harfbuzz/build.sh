0b3fbb4c35d3baa6f8ef10d5247bd355d7c43179
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-05-21 10:46:37 +0100

    added harfbuzz

---
+++ pkgs/harfbuzz/build.sh
@@ -0,0 +1,32 @@
+pkgname=harfbuzz
+pkgver=2.7.4
+
+fetch() {
+	curl -L "https://github.com/harfbuzz/harfbuzz/releases/download/2.7.4/harfbuzz-2.7.4.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib \
+		-Dglib=disabled \
+		-Dgobject=disabled \
+		-Dicu=enabled
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING
+}
