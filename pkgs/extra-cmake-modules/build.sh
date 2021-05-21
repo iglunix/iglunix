b49408406be4b1b69ada339b61c8d997e0ea052b
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-05-21 10:51:01 +0100

    added extra-cmake-modules

---
+++ pkgs/extra-cmake-modules/build.sh
@@ -0,0 +1,30 @@
+pkgname=extra-cmake-modules
+pkgver=5.82.0
+
+fetch() {
+	curl -L "https://download.kde.org/stable/frameworks/5.81/extra-cmake-modules-5.81.0.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	cmake -G Ninja ../ \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX=/usr \
+		-DCMAKE_INSTALL_LIBDIR=lib
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
