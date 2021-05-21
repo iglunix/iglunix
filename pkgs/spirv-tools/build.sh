65d8e1f6d273df63ba0f031c41b707643f7f15e8
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-05-21 10:45:03 +0100

    added spirv-tools

---
+++ pkgs/spirv-tools/build.sh
@@ -0,0 +1,33 @@
+pkgname=spirv-tools
+pkgver=master
+
+fetch() {
+	curl -L "https://github.com/KhronosGroup/SPIRV-Tools/archive/refs/heads/master.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mv SPIRV-Tools-$pkgver spirv-tools-$pkgver
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	cmake -G Ninja ../ \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX=/usr \
+		-DCMAKE_INSTALL_LIBDIR=lib \
+		-DSPIRV-Headers_SOURCE_DIR=/usr \
+		-DSPIRV_WERROR=Off
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
