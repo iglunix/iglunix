76e9750ee83b9404c8958d7f2a86071427dd55f7
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-05-23 10:26:39 +0100

    use sabotage libnl-tiny

--- pkgs/libnl-tiny/build.sh
+++ pkgs/libnl-tiny/build.sh
@@ -2,30 +2,19 @@ pkgname=libnl-tiny
 pkgver=master
 
 fetch() {
-	curl "https://git.openwrt.org/?p=project/libnl-tiny.git;a=snapshot;h=HEAD;sf=tgz" -o $pkgname-$pkgver.tar.xz
+	curl -L "https://github.com/sabotage-linux/libnl-tiny/archive/refs/heads/master.tar.gz" -o $pkgname-$pkgver.tar.xz
 	tar -xf $pkgname-$pkgver.tar.xz
-	cp ../cmake-no-extra-errs.patch .
-	mv $pkgname-HEAD-* $pkgname-$pkgver
-	mkdir $pkgname-$pkgver/build
 	cd $pkgname-$pkgver
-	patch -p1 < ../cmake-no-extra-errs.patch
 }
 
 build() {
 	cd $pkgname-$pkgver
-	cd build
-	cmake -G Ninja ../ \
-		-DCMAKE_BUILD_TYPE=Release \
-		-DCMAKE_INSTALL_PREFIX=/usr \
-		-DCMAKE_INSTALL_LIBDIR=lib
-	samu
+	gmake prefix=/usr all CC=cc
 }
 
 package() {
 	cd $pkgname-$pkgver
-	cd build
-	DESTDIR=$pkgdir samu install
-	ln -sr $pkgdir/usr/lib/pkgconfig/libnl-tiny.pc $pkgdir/usr/lib/pkgconfig/libnl-3.0.pc
+	gmake prefix=/usr DESTDIR=$pkgdir install
 }
 
 license() {
