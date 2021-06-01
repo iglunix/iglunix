0f45579aea9291a3c381c99c56b2d7373e925f5f
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-06-01 13:16:35 +0100

    added openssh

---
+++ pkgs/openssh/build.sh
@@ -0,0 +1,28 @@
+pkgname=openssh
+pkgver=8.6p1
+
+fetch() {
+	curl "https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-$pkgver.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+}
+
+build() {
+	cd $pkgname-$pkgver
+	./configure \
+		--prefix=/usr \
+		--build=x86_64-unknown-linux-musl \
+		--host=x86_64-unknown-linux-musl \
+		--libexecdir=/usr/lib
+
+	make
+}
+
+package() {
+	cd $pkgname-$pkgver
+	make install DESTDIR=$pkgdir
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENCE
+}
