14a4ca1937e8df665401ce5e456b366f7c21ce50
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-06-15 21:01:25 +0100

    added evdev

---
+++ pkgs/libevdev/build.sh
@@ -0,0 +1,31 @@
+pkgname=libevdev
+pkgver=1.10.1
+
+fetch() {
+	curl "https://www.freedesktop.org/software/libevdev/libevdev-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib \
+		-Dtests=disabled \
+		-Ddocumentation=disabled
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING
+}
