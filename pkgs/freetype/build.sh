33e27a4c9ca9b69586ae209ce786607dceec26a2
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-06-15 20:20:56 +0100

    added freetype

---
+++ pkgs/freetype/build.sh
@@ -0,0 +1,30 @@
+pkgname=freetype
+pkgver=2.10.4
+
+fetch() {
+	curl "https://download-mirror.savannah.gnu.org/releases/freetype/freetype-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
