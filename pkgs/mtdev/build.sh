550c78e46076ed9e509936a15d29a145943b8a65
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-06-15 20:55:58 +0100

    added mtdev

---
+++ pkgs/mtdev/build.sh
@@ -0,0 +1,27 @@
+pkgname=mtdev
+pkgver=1.1.6
+
+fetch() {
+	curl "http://bitmath.org/code/mtdev/mtdev-$pkgver.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+}
+
+build() {
+	cd $pkgname-$pkgver
+	./configure \
+		--prefix=/usr \
+		--build=$TRIPLE \
+		--host=$TRIPLE
+
+	make
+}
+
+package() {
+	cd $pkgname-$pkgver
+	make install DESTDIR=$pkgdir
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING
+}
