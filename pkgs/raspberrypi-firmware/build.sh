3a1e88c202203b14bcda775c609917e0d6494d07
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-06-16 22:44:59 +0100

    added rpi firmware

---
+++ pkgs/raspberrypi-firmware/build.sh
@@ -0,0 +1,27 @@
+pkgname=raspberrypi-firmware
+pkgver=20210527
+
+fetch() {
+	curl -L "https://github.com/raspberrypi/firmware/archive/refs/tags/1.$pkgver.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mv firmware-1.$pkgver $pkgname-$pkgver
+}
+
+build() {
+	cd $pkgname-$pkgver
+
+}
+
+package() {
+	cd $pkgname-$pkgver
+	install -d $pkgdir/boot
+	cp ./boot/*.bin $pkgdir/boot	
+	cp ./boot/*.dat $pkgdir/boot
+	cp ./boot/*.elf $pkgdir/boot
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
