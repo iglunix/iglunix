ba57a8f982f02fcd1e3d9cdfcf6881b455784522
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-06-19 12:36:03 +0100

    added reflex

---
+++ pkgs/reflex/build.sh
@@ -0,0 +1,27 @@
+pkgname=reflex
+pkgver=20210510
+
+fetch() {
+	curl "https://invisible-island.net/datafiles/release/reflex.tar.gz" -o $pkgname-$pkgver.tar.gz
+	tar -xf $pkgname-$pkgver.tar.gz
+}
+
+build() {
+	cd $pkgname-$pkgver
+	./configure \
+		--prefix=/usr \
+		--build=$TRIPLE \
+		--host=$TRIPLE
+
+	make
+}
+
+package() {
+	cd $pkgname-$pkgver
+	make install DESTDIR=$pkgdir
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING
+}
