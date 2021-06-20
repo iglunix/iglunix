f045e3dc5cb4d002c5bcbfbca51da4e143c9056c
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-06-20 11:13:21 +0000

    added seatd

---
+++ pkgs/seatd/build.sh
@@ -0,0 +1,32 @@
+pkgname=seatd
+pkgver=0.5.0
+
+fetch() {
+	curl "https://git.sr.ht/~kennylevinsen/seatd/archive/$pkgver.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib \
+		-Dexamples=disabled \
+		-Dc_args=-Wno-sign-compare
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
