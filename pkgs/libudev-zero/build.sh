c59cbaa1c879a5a64eb5c5b6f8f1fdc1e98ca793
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-06-24 21:12:22 +0000

    libudev-zero: add hotplug helper

--- pkgs/libudev-zero/build.sh
+++ pkgs/libudev-zero/build.sh
@@ -9,11 +9,15 @@ fetch() {
 build() {
 	cd $pkgname-$pkgver
 	make
+	cd contrib
+	cc helper.c -o libudev-zero-hotplug-helper
 }
 
 package() {
 	cd $pkgname-$pkgver
 	make install PREFIX=/usr DESTDIR=$pkgdir
+	install -d $pkgdir/usr/sbin
+	install -Dm755 ./contrib/libudev-zero-hotplug-helper $pkgdir/usr/sbin/
 }
 
 license() {
