ec0e9c18f802ab0b555738a3f48a21d041cfb0de
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-06-24 21:09:18 +0000

    added slurp

---
+++ pkgs/slurp/build.sh
@@ -0,0 +1,31 @@
+pkgname=slurp
+pkgver=1.3.2
+deps="wayland:cairo:libxkbcommon"
+
+fetch() {
+	curl -L "https://github.com/emersion/slurp/releases/download/v$pkgver/slurp-$pkgver.tar.gz" -o $pkgname-$pkgver.tar.gz
+	tar -xf $pkgname-$pkgver.tar.gz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
