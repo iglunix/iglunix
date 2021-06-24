95a518736e8ab52f25c016ac32a5558d702e4e73
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-06-24 21:08:56 +0000

    add alsa

---
+++ pkgs/alsa-lib/build.sh
@@ -0,0 +1,28 @@
+pkgname=alsa-lib
+pkgver=1.2.5.1
+
+fetch() {
+	curl -L "http://www.alsa-project.org/files/pub/lib/alsa-lib-$pkgver.tar.bz2" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+}
+
+build() {
+	cd $pkgname-$pkgver
+	./configure \
+		--prefix=/usr \
+		--build=$TRIPLE \
+		--host=$TRIPLE
+
+	make
+}
+
+package() {
+	cd $pkgname-$pkgver
+	make install DESTDIR=$pkgdir
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
