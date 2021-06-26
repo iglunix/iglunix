dac0e1a5e5551d4c605c064d9c3fb34e8776e313
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-06-26 14:42:55 +0000

    add flex again for mesa

---
+++ pkgs/flex/build.sh
@@ -0,0 +1,28 @@
+pkgname=flex
+pkgver=2.6.4
+
+fetch() {
+	curl -L "https://github.com/westes/flex/releases/download/v2.6.4/flex-$pkgver.tar.gz" -o $pkgname-$pkgver.tar.gz
+	tar -xf $pkgname-$pkgver.tar.gz
+}
+
+build() {
+	cd $pkgname-$pkgver
+	./configure \
+		--prefix=/usr \
+		--build=$TRIPLE \
+		--host=$TRIPLE
+
+	make
+}
+
+package() {
+	cd $pkgname-$pkgver
+	make install DESTDIR=$pkgdir
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
