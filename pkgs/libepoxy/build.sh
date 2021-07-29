4875f10fa5e29a4cdd3be98b5b795b22d7a099be
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-07-29 15:05:37 +0000

    Added epoxy

---
+++ pkgs/libepoxy/build.sh
@@ -0,0 +1,31 @@
+pkgname=libepoxy
+pkgver=1.5.8
+
+fetch() {
+	curl -L "https://github.com/anholt/libepoxy/releases/download/$pkgver/libepoxy-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib \
+		-Dglx=no \
+		-Dx11=false
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING
+}
