e52f1879eeea6681898718010e523f5d06b97ccd
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-07-31 12:02:54 +0000

    vulkan-headers: update

--- pkgs/vulkan-headers/build.sh
+++ pkgs/vulkan-headers/build.sh
@@ -1,8 +1,8 @@
 pkgname=vulkan-headers
-pkgver=1.2.166
+pkgver=1.2.182
 
 fetch() {
-	curl -L "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.2.166.tar.gz" -o $pkgname-$pkgver.tar.xz
+	curl -L "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.2.182.tar.gz" -o $pkgname-$pkgver.tar.xz
 	tar -xf $pkgname-$pkgver.tar.xz
 	mv Vulkan-Headers-$pkgver $pkgname-$pkgver
 	mkdir $pkgname-$pkgver/build
