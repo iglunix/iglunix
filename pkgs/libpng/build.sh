b68a480e60cf4ffd1c83de77b7a40b34fd259567
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-07-31 11:55:49 +0000

    added libpng

---
+++ pkgs/libpng/build.sh
@@ -0,0 +1,28 @@
+pkgname=libpng
+pkgver=1.6.37
+
+fetch() {
+	curl -L "https://downloads.sourceforge.net/sourceforge/$pkgname/$pkgname-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+}
+
+build() {
+	cd $pkgname-$pkgver
+	./configure \
+		--prefix=/usr \
+		--build=$TRIPLE \
+		--host=$TRIPLE
+
+	make
+}
+
+package() {
+	cd $pkgname-$pkgver
+	make install DESTDIR=$pkgdir
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
