b710db0adeb516e820781043ce2cfdf2b0adeae8
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-09-16 10:37:33 +0000

    added limine :)

---
+++ linux/limine/build.sh
@@ -0,0 +1,30 @@
+pkgname=limine
+pkgver=2.55
+mkdeps="nasm:llvm"
+
+fetch() {
+	curl -L "https://github.com/limine-bootloader/limine/releases/download/v$pkgver/limine-v$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mv $pkgname-v$pkgver $pkgname-$pkgver
+}
+
+build() {
+	cd $pkgname-$pkgver
+	gmake limine-bios bin/limine-install CC="clang" TOOLCHAIN="llvm" TOOLCHAIN_CC="clang" TOOLCHAIN_LD="ld.lld"
+}
+
+package() {
+	cd $pkgname-$pkgver
+	mkdir -p $pkgdir/usr/sbin/
+	install -m 755 ./bin/limine-install $pkgdir/usr/sbin/
+	mkdir -p $pkgdir/usr/share/limine/
+	install -m 644 ./bin/limine-cd.bin $pkgdir/usr/share/limine/
+	install -m 644 ./bin/limine-hdd.bin $pkgdir/usr/share/limine/
+	install -m 644 ./bin/limine-pxe.bin $pkgdir/usr/share/limine/
+	install -m 644 ./bin/limine.sys $pkgdir/usr/share/limine/
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE.md
+}
