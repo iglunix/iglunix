d84cf4da05a2905037aa89855ba51fcc8dd3e26f
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-09-18 16:24:01 +0000

    perl: move to base, openssl: bump to 3.0.0

--- base/openssl/build.sh
+++ base/openssl/build.sh
@@ -1,5 +1,6 @@
 pkgname=openssl
-pkgver=1.1.1k
+pkgver=3.0.0
+ext="dev"
 
 fetch() {
 	curl "https://www.openssl.org/source/openssl-$pkgver.tar.gz" -o $pkgname-$pkgver.tar.xz
@@ -12,7 +13,7 @@ build() {
 		--prefix=/usr \
 		--openssldir=/etc/ssl \
 		--libdir=lib \
-		linux-$(uname -m) \
+		linux-generic64 \
 		shared no-zlib no-async \
 		no-comp no-idea no-mdc2 \
 		no-rc5 no-ec2m no-sm2 \
@@ -26,10 +27,23 @@ build() {
 package() {
 	cd $pkgname-$pkgver
 	make install_sw DESTDIR=$pkgdir
+	rm -rf $pkgdir/usr/lib/pkgconf
+	rm -rf $pkgdir/usr/include
+}
+
+package_dev() {
+	cd $pkgname-$pkgver
+	make install_sw DESTDIR=$pkgdir
+	rm -rf $pkgdir/usr/bin
 }
 
 license() {
 	cd $pkgname-$pkgver
-	cat LICENSE
-#	cat COPYING
+
+	echo 'Copyright 20xx-20yy The OpenSSL Project Authors. All Rights Reserved.'
+	echo
+	echo 'Licensed under the Apache License 2.0 (the "License").  You may not use'
+	echo 'this file except in compliance with the License.  You can obtain a copy'
+	echo 'in the file LICENSE in the source distribution or at'
+	echo 'https://www.openssl.org/source/license.html'
 }
