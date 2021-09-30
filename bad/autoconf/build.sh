28b6b47c2305e63789f41a06754c07570c638c08
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-09-30 13:45:58 +0100

    added autoconf

---
+++ bad/autoconf/build.sh
@@ -0,0 +1,31 @@
+pkgname=autoconf
+pkgver=2.13
+
+fetch() {
+	curl "https://ftp.gnu.org/gnu/autoconf/autoconf-$pkgver.tar.gz" -o $pkgname-$pkgver.tar.gz
+	tar -xf $pkgname-$pkgver.tar.gz
+	cd $pkgname-$pkgver
+	patch -p1 < ../../makefile-m4f.patch
+	patch -p1 < ../../autoconf.sh.patch
+	patch -p1 < ../../autoheader.sh.patch
+}
+
+build() {
+	cd $pkgname-$pkgver
+	./configure \
+		--prefix=/usr \
+		--build=$TRIPLE \
+		--host=$TRIPLE
+
+	make
+}
+
+package() {
+	cd $pkgname-$pkgver
+	make install DESTDIR=$pkgdir
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING
+}
