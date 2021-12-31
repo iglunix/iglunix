14c970f83142d71d3412313000169f3eb86e875d
Ella Stanforth <23418164+Ella-0@users.noreply.github.com>
2021-12-31 12:35:39 +0000

    add gdk-pixbuf

---
+++ gnome/gdk-pixbuf/build.sh
@@ -0,0 +1,45 @@
+pkgname=gdk-pixbuf
+_pkgver=2.42
+pkgver=$_pkgver.6
+
+fetch() {
+	curl -L "https://download.gnome.org/sources/gdk-pixbuf/$_pkgver/gdk-pixbuf-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+	rm $pkgdir-$pkgver/subprojects/*.wrap
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib \
+		-Ddebug=false \
+		-Djpeg=true \
+		-Dpng=true \
+		-Dtiff=false \
+		-Ddocs=false \
+		-Dman=false \
+		-Dgio_sniffing=false \
+		-Dinstalled_tests=false \
+		-Dbuiltin_loaders=all
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+backup() {
+	return
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
