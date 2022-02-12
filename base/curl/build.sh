cfb8c64e697b610ff3ac353e7005cad4f50948c5
Ella Stanforth <ella@iglunix.org>
2022-02-12 13:31:24 +0000

    bump curl

--- base/curl/build.sh
+++ base/curl/build.sh
@@ -1,11 +1,10 @@
-pkgver=7.79.0
+pkgver=7.81.0
 pkg_ver=$(echo $pkgver | tr '.' '_')
 pkgname=curl
 pkgrel=1
 mkdeps="samu:cmake"
 deps="openssl"
 bad=""
-ext="doc:dev"
 
 fetch() {
 	curl -L "https://github.com/curl/curl/releases/download/curl-$pkg_ver/curl-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.gz
@@ -19,7 +18,8 @@ build() {
 	cmake -G Ninja ../ \
 		-DCMAKE_BUILD_TYPE=Release \
 		-DCMAKE_INSTALL_PREFIX=/usr \
-		-DCMAKE_INSTALL_LIBDIR=lib
+		-DCMAKE_INSTALL_LIBDIR=lib \
+		-DCURL_CA_BUNDLE="/etc/ssl/cert.pem"
 	samu
 }
 
@@ -27,20 +27,10 @@ package() {
 	cd $pkgname-$pkgver
 	cd build
 	DESTDIR=$pkgdir samu install
-	rm -r $pkgdir/usr/share
-	rm -r $pkgdir/usr/include
-	rm -r $pkgdir/usr/lib/pkgconfig
-	rm -r $pkgdir/usr/lib/cmake
 }
 
-package_dev() {
-	cd $pkgname-$pkgver
-	cd build
-	DESTDIR=$pkgdir samu install
-	rm -r $pkgdir/usr/share
-	rm -r $pkgdir/usr/bin
-	rm $pkgdir/usr/lib/*.so
-	rm $pkgdir/usr/lib/*.so.*
+backup() {
+	return
 }
 
 license() {
