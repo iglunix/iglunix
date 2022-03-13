af3aacaebc00cbc322b7e2c613129d46104a4d89
Ella Stanforth <ella@iglunix.org>
2022-03-13 12:03:56 +0000

    foot: fix terminfo

--- gui/foot/build.sh
+++ gui/foot/build.sh
@@ -2,10 +2,16 @@ pkgname=foot
 pkgver=1.10.3
 
 fetch() {
-	curl -L "https://codeberg.org/dnkl/foot/archive/$pkgver.tar.gz" -o $pkgname-$pkgver.tar.xz
-	tar -xf $pkgname-$pkgver.tar.xz
+	curl -L "https://codeberg.org/dnkl/foot/archive/$pkgver.tar.gz" -o $pkgname-$pkgver.tar.gz
+	curl -L "https://codeberg.org/dnkl/fcft/archive/2.4.5.tar.gz" -o fcft.tar.gz
+	curl -L "https://codeberg.org/dnkl/tllist/archive/1.0.5.tar.gz" -o tllist.tar.gz
+	tar -xf $pkgname-$pkgver.tar.gz
 	mv $pkgname $pkgname-$pkgver
 	mkdir $pkgname-$pkgver/build
+	tar -xf fcft.tar.gz -C $pkgname-$pkgver/subprojects
+	tar -xf tllist.tar.gz -C $pkgname-$pkgver/subprojects
+	cd $pkgname-$pkgver
+	patch -p1 < ../../netbsd-tic.patch
 }
 
 build() {
@@ -15,8 +21,8 @@ build() {
 		--buildtype=release \
 		--prefix=/usr \
 		--libexecdir=lib \
-		-Dterminfo=disabled \
-		-Ddefault-terminfo=xterm
+		-Dterminfo=enabled \
+		-Ddefault-terminfo=foot
 	samu
 }
 
