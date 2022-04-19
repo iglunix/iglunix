c195af991693c3c3d64112805ca1f2b630077a1e
Ella Stanforth <ella@iglunix.org>
2022-04-19 18:55:50 +0000

    libunwind: use CXX and CC

--- base/libunwind/build.sh
+++ base/libunwind/build.sh
@@ -1,4 +1,4 @@
-pkgver=14.0.0
+pkgver=14.0.1
 pkgname=libunwind
 bad=""
 ext="dev"
@@ -14,9 +14,9 @@ build() {
 
 	PREFIX=/usr
 
-	[ -z "$FOR_CROSS"] || PREFIX=$FOR_CROSS_DIR
+	[ -z "$FOR_CROSS" ] || PREFIX=$FOR_CROSS_DIR
 
-	if [ ! -z "$WITH_CROSS"]; then
+	if [ ! -z "$WITH_CROSS" ]; then
 		cmake_extra_flags=-DCMAKE_SYSROOT=$WITH_CROSS_DIR
 	fi
 
@@ -28,9 +28,9 @@ build() {
 		-DLIBUNWIND_SUPPORTS_FNO_EXCEPTIONS_FLAG=1 \
 		-DLIBCXXABI_USE_LLVM_UNWINDER=YES \
 		-DLIBCXX_HAS_MUSL_LIBC=ON \
-		-DCMAKE_ASM_COMPILER=clang \
-		-DCMAKE_C_COMPILER=clang \
-		-DCMAKE_CXX_COMPILER=clang++ \
+		-DCMAKE_ASM_COMPILER=$CC \
+		-DCMAKE_C_COMPILER=$CC \
+		-DCMAKE_CXX_COMPILER=$CXX \
 		-DCMAKE_ASM_COMPILER_TARGET=$ARCH-linux-musl \
 		-DCMAKE_C_COMPILER_TARGET=$ARCH-linux-musl \
 		-DCMAKE_CXX_COMPILER_TARGET=$ARCH-linux-musl \
