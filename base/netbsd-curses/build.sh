4c80b29903162d3f0c9d6ecc5830ef0881ace2c7
Ella Stanforth <ella@iglunix.org>
2022-04-21 11:22:58 +0000

    netbsd-curses: fix cross

--- base/netbsd-curses/build.sh
+++ base/netbsd-curses/build.sh
@@ -3,15 +3,18 @@ pkgname=netbsd-curses
 bad="gmake"
 deps="musl"
 ext="doc:dev"
+auto_cross
 
 fetch() {
 	curl -L "https://github.com/sabotage-linux/netbsd-curses/archive/master.tar.gz" -o $pkgname-$pkgver.tar.gz
 	tar -xf $pkgname-$pkgver.tar.gz
+	cd $pkgname-$pkgver
+	patch -p1 < ../../cross.patch
 }
 
 build() {
 	cd $pkgname-$pkgver
-	bad --gmake gmake PREFIX=/usr
+	bad --gmake gmake PREFIX=/usr CROSSCOMPILING=1 LDFLAGS_HOST= CFLAGS_HOST=
 }
 
 package() {
