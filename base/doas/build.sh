969b87569ec04e3f9d29dfc41d06235bee7dd760
Ella Stanforth <ella@iglunix.org>
2022-04-22 20:53:43 +0000

    doas: fix build

--- base/doas/build.sh
+++ base/doas/build.sh
@@ -3,9 +3,10 @@ pkgname=doas
 pkgrel=1
 bad="gmake"
 ext="doc"
+auto_cross
 
 fetch() {
-	curl -L "https://github.com/Duncaen/OpenDoas/releases/download/v6.8/opendoas-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
+	curl -L "https://github.com/Duncaen/OpenDoas/releases/download/v$pkgver/opendoas-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
 	tar -xf $pkgname-$pkgver.tar.xz
 	mv opendoas-$pkgver $pkgname-$pkgver
 	cp ../build.ninja $pkgname-$pkgver
@@ -15,25 +16,21 @@ build() {
 	cd $pkgname-$pkgver
 	./configure --prefix=/usr \
 	--without-pam
-	gmake
+	bad --gmake gmake
 }
 
 package() {
 	cd $pkgname-$pkgver
 	install -d $pkgdir/usr/bin
-	install -Dm4755 doas $pkgdir/usr/bin
-}
-
-package_doc() {
-	cd $pkgname-$pkgver
-	install -d $pkgdir/usr/share/man/
-	install -d $pkgdir/usr/share/man/man1
-	install -d $pkgdir/usr/share/man/man5
-	install -Dm644 doas.1 $pkgdir/usr/share/man/man1
-	install -Dm644 doas.conf.5 $pkgdir/usr/share/man/man5
+	install -Dm755 doas $pkgdir/usr/bin
+	chmod u+s $pkgdir/usr/bin/doas
 }
 
 license() {
 	cd $pkgname-$pkgver
 	cat LICENSE
+}
+
+backup() {
+	return
 }
