71e913832358c6dd44c058071c1d91037f4666bd
Ella Stanforth <ella@iglunix.org>
2022-09-09 18:53:34 +0000

    add basu

---
+++ gui/basu/build.sh
@@ -0,0 +1,36 @@
+pkgname=basu
+pkgver=0.2.0
+
+fetch() {
+	curl "https://git.sr.ht/~emersion/basu/refs/download/v$pkgver/basu-$pkgver.tar.gz" -o $pkgname-$pkgver.tar.gz
+	tar -xf $pkgname-$pkgver.tar.gz
+	mkdir $pkgname-$pkgver/build
+	cd $pkgname-$pkgver
+	patch -p1 < ../../0001-remove-gperf.patch
+	patch -p1 < ../../bus-error.patch
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE*
+}
+
+backup() {
+	return
+}
