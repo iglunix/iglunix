c63cecdc9eb12c60712397ffa1949b9a9c753ba3
Ella Stanforth <ella@iglunix.org>
2022-09-09 18:42:33 +0000

    libportal

---
+++ gui/libportal/build.sh
@@ -0,0 +1,36 @@
+pkgname=libportal
+pkgver=0.6
+
+fetch() {
+	curl -L "https://github.com/flatpak/libportal/releases/download/$pkgver/libportal-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib \
+		-Dintrospection=false \
+		-Dbackends=gtk3 \
+		-Ddocs=false
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING
+}
+
+backup() {
+	return
+}
