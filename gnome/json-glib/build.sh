00c06a8ec141aab27033c904b086695857a3fdde
Ella Stanforth <ella@iglunix.org>
2022-09-09 18:36:54 +0000

    json-glib

---
+++ gnome/json-glib/build.sh
@@ -0,0 +1,33 @@
+pkgname=json-glib
+pkgver=1.6.6
+
+fetch() {
+	curl -L "https://download.gnome.org/sources/json-glib/1.6/json-glib-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	meson .. \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING
+}
+
+backup() {
+	return
+}
