1dc79fbc0a43d40df799769b46691d4dd5847c4c
Ella Stanforth <ella@iglunix.org>
2022-09-10 11:28:30 +0000

    add libjpeg turbo

---
+++ gui/libjpeg-turbo/build.sh
@@ -0,0 +1,33 @@
+pkgname=libjpeg-turbo
+pkgver=2.1.2
+
+fetch() {
+	curl -L "https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/$pkgver.tar.gz" -o $pkgname-$pkgver.tar.gz
+	tar -xf $pkgname-$pkgver.tar.gz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	cmake -G Ninja ../ \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX=/usr \
+		-DCMAKE_INSTALL_LIBDIR=/usr/lib
+	samu
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+backup() {
+	return
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE.md
+}
