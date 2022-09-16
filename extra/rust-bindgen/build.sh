4aaa85298775d3b5c6a92c4bda4b581c08d6d392
Ella Stanforth <ella@iglunix.org>
2022-09-16 14:48:30 +0000

    add rust-bindgen

---
+++ extra/rust-bindgen/build.sh
@@ -0,0 +1,34 @@
+pkgname=rust-bindgen
+pkgver=0.60.1
+
+_clear_vendor_checksums() {
+	sed -i 's/\("files":{\)[^}]*/\1/' vendor/$1/.cargo-checksum.json
+}
+
+fetch() {
+	curl -L "https://github.com/rust-lang/rust-bindgen/archive/refs/tags/v$pkgver.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	cd $pkgname-$pkgver
+	mkdir -p .cargo
+	cargo vendor > .cargo/config
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cargo build --release --locked
+}
+
+package() {
+	cd $pkgname-$pkgver
+	install -Dm755 target/release/bindgen $pkgdir/usr/bin/bindgen
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
+
+backup() {
+	return
+}
