493d674e75cb7564690ca65c524dd4ee9d506ece
Ella Stanforth <ella@iglunix.org>
2022-09-16 14:48:19 +0000

    add rust-analyzer

---
+++ extra/rust-analyzer/build.sh
@@ -0,0 +1,33 @@
+pkgname=rust-analyzer
+pkgver=2022-09-12
+
+_clear_vendor_checksums() {
+	sed -i 's/\("files":{\)[^}]*/\1/' vendor/$1/.cargo-checksum.json
+}
+
+fetch() {
+	curl -L "https://github.com/rust-lang/rust-analyzer/archive/refs/tags/$pkgver.tar.gz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	cd $pkgname-$pkgver
+	mkdir -p .cargo
+	cargo vendor > .cargo/config
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cargo build --release --locked
+}
+
+package() {
+	cd $pkgname-$pkgver
+	install -Dm755 target/release/$pkgname $pkgdir/usr/bin/$pkgname
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE*
+}
+
+backup() {
+	return
+}
