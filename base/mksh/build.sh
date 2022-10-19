6155931fdc0ba38128c204a33d53658306c7084a
Ella Stanforth <ella@iglunix.org>
2022-10-19 19:39:12 +0000

    mksh: add <() substitution

--- base/mksh/build.sh
+++ base/mksh/build.sh
@@ -7,16 +7,19 @@ ext="doc"
 fetch() {
 	curl http://www.mirbsd.org/MirOS/dist/mir/mksh/mksh-R59c.tgz -o $pkgname-$pkgver.tar.gz
 	tar -xf $pkgname-$pkgver.tar.gz
+	mv $pkgname $pkgname-$pkgver
+	cd $pkgname-$pkgver
+	patch -p1 < ../../procsub.patch
 }
 
 build() {
-	cd $pkgname
+	cd $pkgname-$pkgver
 	CPPFLAGS="-DMKSH_BINSHPOSIX" sh Build.sh -L
 	sh Build.sh
 }
 
 package() {
-	cd $pkgname
+	cd $pkgname-$pkgver
 	install -d $pkgdir/bin
 	install -Dm755 ./mksh $pkgdir/bin
 	install -Dm755 ./lksh $pkgdir/bin
