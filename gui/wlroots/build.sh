1d6f68b6d94a604857028b8d26ff8621f58be428
aheirman <48590738+aheirman@users.noreply.github.com>
2023-01-15 21:34:14 +0100

    add dep to wlroots

--- gui/wlroots/build.sh
+++ gui/wlroots/build.sh
@@ -1,6 +1,6 @@
 pkgname=wlroots
 pkgver=0.16.1
-deps="libdrm"
+deps="libdrm:libxkbcommon"
 
 fetch() {
 	curl -L "https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/$pkgver/wlroots-$pkgver.tar.gz" -o $pkgname-$pkgver.tar.xz
