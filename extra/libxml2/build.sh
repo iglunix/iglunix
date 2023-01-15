febf1bcdc0c9ebc3ac52d34fc67ff6a905836fd1
aheirman <48590738+aheirman@users.noreply.github.com>
2023-01-15 21:50:55 +0100

    fix & update libxml2

--- extra/libxml2/build.sh
+++ extra/libxml2/build.sh
@@ -1,30 +1,35 @@
-pkgver=2.9.10
+pkgver=2.10.3
 pkgname=libxml2
 bad=""
 ext="dev"
 
 fetch() {
-	curl "ftp://xmlsoft.org/libxml2/libxml2-2.9.10.tar.gz" -o $pkgname-$pkgver.tar.gz
-	tar -xf $pkgname-$pkgver.tar.gz
+    curl -L "https://download.gnome.org/sources/libxml2/2.10/libxml2-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.gz
+    tar -xf $pkgname-$pkgver.tar.gz
+}
+
+backup() {
+    return
 }
 
 build() {
-	cd $pkgname-$pkgver
-	./configure --prefix=/usr --without-python
-	bad --gmake gmake
+    cd $pkgname-$pkgver
+    ./configure --prefix=/usr --without-python
+    bad --gmake gmake
 }
 
 package() {
-	cd $pkgname-$pkgver
-	bad --gmake gmake install DESTDIR=$pkgdir
+    cd $pkgname-$pkgver
+    bad --gmake gmake install DESTDIR=$pkgdir
 }
 
 package_dev() {
-	cd $pkgname-$pkgver
-	bad --gmake gmake install DESTDIR=$pkgdir
+    cd $pkgname-$pkgver
+    bad --gmake gmake install DESTDIR=$pkgdir
 }
 
 license() {
-	cd $pkgname-$pkgver
-	cat Copyright
+    cd $pkgname-$pkgver
+    cat Copyright
 }
+
