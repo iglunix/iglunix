995163b24edf1ce562bf349b64488923d8a683b4
aheirman <48590738+aheirman@users.noreply.github.com>
2023-01-24 09:09:49 +0000

    add hack font

---
+++ fonts/hack/build.sh
@@ -0,0 +1,30 @@
+pkgname=hack
+pkgver=3.003                                                                                                         
+                                                                                                                     
+fetch() {                                                                                                            
+        mkdir $pkgname-$pkgver         
+        cd $pkgname-$pkgver
+	curl -L "https://github.com/source-foundry/Hack/releases/download/v$pkgver/Hack-v$pkgver-ttf.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz                                                                              
+                                                                                                                     
+}                                                                                                                    
+                                                                                                                     
+build() {                                                                                                            
+	cd $pkgname-$pkgver
+}                                                                                                                    
+                                                                                                                     
+backup() {                                                                                                           
+}                                                                                                                    
+                                                                                                                     
+package() {                                                                                                          
+        cd $pkgname-$pkgver                                                                                          
+        mkdir -p $pkgdir/usr/share/fonts                                                                                                         
+        mv ./* $pkgdir/usr/share/fonts                                                                          
+}                                                                                                                    
+                                                                                                                     
+license() {                                                                                                          
+        cd $pkgname-$pkgver                                                                                          
+#       cat LICENSE                                                                                                  
+#       cat COPYING
+	echo "MIT"                                                                                                  
+}           
