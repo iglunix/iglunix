20996cbf6798a4376e8743eb3edd46951c8e0f01
aheirman <48590738+aheirman@users.noreply.github.com>
2023-01-24 14:43:26 +0000

    add dbus

---
+++ gnome/dbus/build.sh
@@ -0,0 +1,33 @@
+pkgname=dbus
+pkgver=1.15.2
+
+fetch() {
+	curl "https://dbus.freedesktop.org/releases/dbus/dbus-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	./configure \
+		--prefix=/usr \
+		--sysconfdir=/etc \
+		--build=$TRIPLE \
+		--host=$TRIPLE
+
+	bad --gmake gmake
+}
+
+package() {
+	cd $pkgname-$pkgver
+	bad --gmake gmake install DESTDIR=$pkgdir
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSES/*
+}
+
+backup() {
+	return
+}
