bb5be443e89e1e440757fb6b073917e2b818f24e
Ella Stanforth <ella@iglunix.org>
2023-03-30 04:52:36 +0100

    comment out broken patch

--- base/toybox/build.sh
+++ base/toybox/build.sh
@@ -10,7 +10,7 @@ fetch() {
 	cd $pkgname-$pkgver
 	patch -p1 < ../../ls-colour.patch
 	patch -p1 < ../../mksh.patch
-	patch -p1 < ../../mksh2
+	# patch -p1 < ../../mksh2
 	patch -p1 < ../../xxd-i.patch
 }
 
