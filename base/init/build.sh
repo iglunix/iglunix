5ab5d881a62ab62aa7782f829b55280d880d43f6
Ella Stanforth <ella@iglunix.org>
2023-04-03 05:30:54 +0100

    add init

---
+++ base/init/build.sh
@@ -0,0 +1,25 @@
+pkgname=init
+pkgver=main
+
+fetch() {
+	curl "https://github.com/iglunix/init/archive/refs/heads/$pkgver.tar.gz" -o $pkgname-$pkgver.tar.gz
+	tar -xf $pkgname-$pkgver.tar.gz
+}
+
+build() {
+	cd $pkgname-$pkgver
+}
+
+package() {
+	cd $pkgname-$pkgver
+	make install DESTDIR=$pkgdir
+	cp -r etc $pkgdir
+	cp -r lib $pkgdir
+	cp -r sbin $pkgdir
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
