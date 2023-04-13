0648491081ee2fa185bfe26d0b506ef220474e68
Ella Stanforth <ella@iglunix.org>
2023-04-13 12:09:34 +0000

    add checkbox on out of date package issues

--- scripts/repover.sh
+++ scripts/repover.sh
@@ -47,9 +47,9 @@ fi
 if [ "$REPO_MAJOR" -gt "$LOCAL_MAJOR" -o "$REPO_MINOR" -gt "$LOCAL_MINOR" -o "$REPO_MICRO" -gt "$LOCAL_MICRO" ]
 then
 	printf '\n'
-	printf ' - Package `%s` is out of date!\n' "$1/$2"
-	printf '   Remote Version: `%s.%s.%s`\n' $REPO_MAJOR $REPO_MINOR $REPO_MICRO
-	printf '   Local Version: `%s.%s.%s`\n' $LOCAL_MAJOR $LOCAL_MINOR $LOCAL_MICRO
+	printf ' - [ ] Package `%s` is out of date!\n' "$1/$2"
+	printf '       Remote Version: `%s.%s.%s`\n' $REPO_MAJOR $REPO_MINOR $REPO_MICRO
+	printf '       Local Version: `%s.%s.%s`\n' $LOCAL_MAJOR $LOCAL_MINOR $LOCAL_MICRO
 	printf '\n'
 fi
 
