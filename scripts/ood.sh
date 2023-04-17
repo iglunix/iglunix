be15d2f1ff0a728cd2088da78b79c5c368d485b7
Ella Stanforth <ella@iglunix.org>
2023-04-17 15:29:18 +0100

    ood: add more packages

--- scripts/ood.sh
+++ scripts/ood.sh
@@ -6,6 +6,19 @@ CV=$RR/scripts/repover.sh
 #printf 'title: Out of date packages %s\n' "$(date -I)"
 #printf -- '---\n'
 
+
+# gmake is not the upstream package name
+# $CV bad gmake 2>/dev/null
+
+# mksh versions are not integers
+# $CV base mksh 2>/dev/null
+
+# flex repover is not right
+# $CV base flex 2>/dev/null
+
+$CV linux musl 2>/dev/null
+$CV linux toybox 2>/dev/null
+$CV linux busybox 2>/dev/null
 $CV base bmake 2>/dev/null
 $CV base byacc 2>/dev/null
 $CV base dhcpcd 2>/dev/null
