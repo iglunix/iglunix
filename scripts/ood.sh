4e3e522d35dc7d68b1bb3c237e6187b97dce4c09
Ella Stanforth <ella@iglunix.org>
2023-04-17 12:51:02 +0100

    add a couple packages to ood

--- scripts/ood.sh
+++ scripts/ood.sh
@@ -6,5 +6,8 @@ CV=$RR/scripts/repover.sh
 #printf 'title: Out of date packages %s\n' "$(date -I)"
 #printf -- '---\n'
 
+$CV base bmake 2>/dev/null
+$CV base byacc 2>/dev/null
+$CV base dhcpcd 2>/dev/null
 $CV base openssl 2>/dev/null
 $CV base llvm 2>/dev/null
