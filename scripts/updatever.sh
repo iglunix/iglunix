06f13ddae3d50e27431950ae00bf4ab102cc5e68
Ella Stanforth <ella@iglunix.org>
2023-04-17 14:01:35 +0100

    improve updatever

--- scripts/updatever.sh
+++ scripts/updatever.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-NVER=$(./repover.sh $1)
+NVER=$(curl "https://repology.org/project/$2/history" 2>/dev/null | grep "version-newest" | tr '>' ' ' | tr '<' ' ' | awk '{ print $5; }' | head -n1)
 TMP=$(mktemp)
-sed 's/pkgver=.*/pkgver='$NVER'/g' pkgs/$1/build.sh > $TMP
-mv $TMP pkgs/$1/build.sh
+sed 's/pkgver=.*/pkgver='$NVER'/' $1/$2/build.sh > $TMP
+mv $TMP $1/$2/build.sh
