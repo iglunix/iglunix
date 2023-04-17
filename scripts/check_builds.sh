b7553c6a2ab20daf694a1915504696667f2239f5
Ella Stanforth <ella@iglunix.org>
2023-04-17 13:30:12 +0100

    starting on automated build checks

---
+++ scripts/check_builds.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+
+if [ ! -d ./sysroot ]
+then
+	printf 'ERROR: sysroot not setup'
+	false
+fi
+
+if [ ! -d "$1" ]
+then
+	printf 'ERROR: package %s does not exist' "$1"
+	false
+fi
+
+BASE=$(pwd)
+SYSROOT=$BASE/sysroot
+IGLUPKG=$BASE/sysroot/usr/sbin/iglupkg
+cd "$1"
+
+$IGLUPKG f
+
+cd $BASE
+
+sudo chroot ./sysroot /build/scripts/check_builds_chroot.sh
---
+++ scripts/setup_sysroot.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -e
+
+if [ ! -f pkgs.tar.zst ]
+then
+	printf 'ERROR: must download pkgs.tar.zst first'
+	false
+fi
+
+mkdir -p sysroot
+
+for pkg in $(tar -tf pkgs.tar.zst)
+do
+	tar -xf pkgs.tar.zst $pkg -C /tmp
+	tar -xf /tmp/$pkg -C sysroot
+	rm -f /tmp/$pkg
+done
+
+mkdir -p sysroot/tmp
+mkdir -p sysroot/proc
+mkdir -p sysroot/sys
+mkdir -p sysroot/dev
+mkdir -p sysroot/build
+mount --bind /tmp sysroot/tmp
+mount --bind /proc sysroot/proc
+mount --bind /sys sysroot/sys
+mount --bind /dev sysroot/dev
+mount --bind $(pwd) sysroot/build
