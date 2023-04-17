f0d7fa9539794c6e6ba5d6f4220353c04d5963e1
Ella Stanforth <ella@iglunix.org>
2023-04-17 14:37:51 +0100

    trace updatever

--- scripts/updatever.sh
+++ scripts/updatever.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -ex
 NVER=$(curl "https://repology.org/project/$2/history" 2>/dev/null | grep "version-newest" | tr '>' ' ' | tr '<' ' ' | awk '{ print $5; }' | head -n1)
 TMP=$(mktemp)
 sed 's/pkgver=.*/pkgver='$NVER'/' $1/$2/build.sh > $TMP
