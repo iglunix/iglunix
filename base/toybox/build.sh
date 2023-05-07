ab7574765bf280dfcf2f095e069dc7305f1d0788
Ella Stanforth <ella@iglunix.org>
2023-05-07 11:28:22 +0000

    base/toybox: use hwdata

--- base/toybox/build.sh
+++ base/toybox/build.sh
@@ -6,19 +6,20 @@ deps="musl"
 fetch() {
 	curl -O "https://landley.net/toybox/downloads/toybox-$pkgver.tar.gz"
 	tar -xf $pkgname-$pkgver.tar.gz
-	curl "https://pci-ids.ucw.cz/v2.2/pci.ids" -o pci.ids
 	cd $pkgname-$pkgver
 	patch -p1 < ../../ls-colour.patch
 	patch -p1 < ../../mksh.patch
 	patch -p1 < ../../mksh2.patch
 	patch -p1 < ../../xxd-i.patch
+
+	sed -i 's|/usr/share/misc|/usr/share/hwdata|' toys/other/lsusb.c
 }
 
 build() {
 	cd $pkgname-$pkgver
 	CPUS=1 bad --gmake gmake defconfig
-	sed 's|# CONFIG_SH is not set|CONFIG_SH=y|' .config > /tmp/_
-	mv /tmp/_ .config
+	# sed 's|# CONFIG_SH is not set|CONFIG_SH=y|' .config > /tmp/_
+	# mv /tmp/_ .config
 	CPUS=1 bad --gmake gmake
 }
 
@@ -27,9 +28,6 @@ backup() {
 }
 
 package() {
-	install -d $pkgdir/usr/share/misc
-	install -Dm 644 pci.ids $pkgdir/usr/share/misc
-
 	cd $pkgname-$pkgver
 	bad --gmake gmake PREFIX=$pkgdir install
 
@@ -42,8 +40,8 @@ package() {
 #	rm $pkgdir/usr/bin/tar
 
 	# MKSH provides this
-	rm $pkgdir/bin/sh
-	rm $pkgdir/bin/bash
+	#rm $pkgdir/bin/sh
+	#rm $pkgdir/bin/bash
 }
 
 license() {
