3706ab6bccf2c0651e753185d3e60abdde44b639
Ella Stanforth <ella@iglunix.org>
2023-05-07 10:59:58 +0000

    add muon

---
+++ base/muon/build.sh
@@ -0,0 +1,38 @@
+pkgname=muon
+pkgver=0.2.0
+
+fetch() {
+	curl -L "https://git.sr.ht/~lattis/muon/archive/$pkgver.tar.gz" -o $pkgname-$pkgver.tar.gz
+	tar -xf $pkgname-$pkgver.tar.gz
+}
+
+build() {
+	cd $pkgname-$pkgver
+	./bootstrap.sh build
+	./build/muon setup \
+		-D prefix=/usr \
+		-D libcurl=disabled \
+		-D libarchive=disabled \
+		-D libpkgconf=enabled \
+		-D samurai=disabled \
+		-D bestline=enabled \
+		build
+
+	samu -C build
+
+	build/muon -C build test
+}
+
+package() {
+	cd $pkgname-$pkgver
+	DESTDIR=$pkgdir build/muon -C build install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSES/GPL-3.0-only.txt
+}
+
+backup() {
+	return
+}
