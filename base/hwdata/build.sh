64b128d961bd0ea68ea5b8c2ebe372bde33eed1d
Ella Stanforth <ella@iglunix.org>
2023-05-07 11:17:02 +0000

    add hwdata

---
+++ base/hwdata/build.sh
@@ -0,0 +1,32 @@
+pkgname=hwdata
+pkgver=0.370
+
+fetch() {
+	curl -L "https://github.com/vcrhonek/hwdata/archive/refs/tags/v$pkgver.tar.gz" -o $pkgname-$pkgver.tar.gz
+	tar -xf $pkgname-$pkgver.tar.gz
+}
+
+build() {
+	cd $pkgname-$pkgver
+	./configure \
+		--prefix=/usr \
+		--sysconfdir=/etc \
+		--disable-blacklist
+
+	bad --gmake gmake
+}
+
+package() {
+	cd $pkgname-$pkgver
+	bad --gmake gmake install DESTDIR=$pkgdir
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+#	cat COPYING
+}
+
+backup() {
+	return
+}
