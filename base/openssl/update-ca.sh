780c4f36e2c1cd1e3cfe7822fe644d1b0d3620a9
Ella Stanforth <ella@iglunix.org>
2023-05-07 14:02:33 +0000

    improve update-ca.sh

--- base/openssl/update-ca.sh
+++ base/openssl/update-ca.sh
@@ -1,2 +1,15 @@
-#!/bin/sh
-curl -L https://curl.haxx.se/ca/cacert.pem -o /etc/ssl/cert.pem
+#!/bin/sh -e
+
+if [ ! "$(id -u)" -eq 0 ]
+then
+	printf 'ERROR: must be run as root\n'
+	exit 1
+fi
+
+extra_args=
+if [ ! -e /etc/ssl/cert.pem ]
+then
+	printf 'WARNING: using insecure protocol for downloading certificates\n'
+	extra_args=-k
+fi
+curl $extra_args -L https://curl.haxx.se/ca/cacert.pem -o /etc/ssl/cert.pem
