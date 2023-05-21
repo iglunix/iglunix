32f2405fe5402a956e5e98195d1a04ee4fe7f88c
Ella Stanforth <ella@iglunix.org>
2023-05-21 14:53:04 +0000

    libglvnd: fix deps

--- gui/libglvnd/build.sh
+++ gui/libglvnd/build.sh
@@ -1,28 +1,31 @@
 pkgname=libglvnd
 pkgver=master
+mkdeps=samurai:muon:pkgconf
 
 fetch() {
 	curl -L "https://github.com/NVIDIA/libglvnd/archive/refs/heads/master.tar.gz" -o $pkgname-$pkgver.tar.xz
 	tar -xf $pkgname-$pkgver.tar.xz
 	mkdir $pkgname-$pkgver/build
+	cd $pkgname-$pkgver
+	sed -i "s|if not gl_dispatch_type.endswith('_tls')|if false|" meson.build
 }
 
 build() {
 	cd $pkgname-$pkgver
-	cd build
-	meson .. \
-		--buildtype=release \
-		--prefix=/usr \
-		--libexecdir=lib \
-		--libdir=lib \
-		-Dtls=false
-	samu
+	muon setup \
+		-Dbuildtype=release \
+		-Ddefault_library=shared \
+		-Dprefix=/usr \
+		-Dlibexecdir=lib \
+		-Dlibdir=lib \
+		-Dtls=false \
+		build
+	samu -C build
 }
 
 package() {
 	cd $pkgname-$pkgver
-	cd build
-	DESTDIR=$pkgdir samu install
+	DESTDIR=$pkgdir muon -C build install
 }
 
 license() {
