9fab4e053f1ee8fee221417ade6adf5dff63baad
Ella Stanforth <ella@iglunix.org>
2023-05-21 09:11:03 +0000

    check_builds: fix output

--- scripts/check_builds.sh
+++ scripts/check_builds.sh
@@ -23,14 +23,18 @@ cd $BASE
 
 sudo chroot ./sysroot /build/scripts/check_builds_chroot.sh "$1"
 
+mkdir -p out
+
 if [ -e "$1"/out/*.tar ]
 then
-	zstd --ultra -22 "$1"/out/*.tar
+	zstd --ultra -22 "$1"/out/*.tar -o out/$(basename "$1").tar.zst
+else
+	cp "$1"/out/*.tar.zst out/$(basename "$1").tar.zst
 fi
 
 if [ -e ~/.ssh/mirror.key ]
 then
 	scp -i ~/.ssh/mirror.key \
-	"$1"/out/*.tar.zst \
+	out/$(basename "$1").tar.zst \
 	root@mirror.iglunix.org:/srv/http/mirror/x86_64/$(basename "$1").tar.zst
 fi
