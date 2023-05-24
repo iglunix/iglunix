dab75936f5aec6d3cd5ecf3d0f97116ea8e4d937
Ella Stanforth <ella@iglunix.org>
2023-05-24 14:15:24 +0000

    wayland: add expat dep

--- gui/wayland/build.sh
+++ gui/wayland/build.sh
@@ -1,7 +1,7 @@
 pkgname=wayland
 pkgver=1.21.0
 mkdeps=muon:samurai:pkgconf
-deps=libffi
+deps=libffi:expat
 
 fetch() {
 	curl -L "https://gitlab.freedesktop.org/wayland/wayland/-/releases/$pkgver/downloads/wayland-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
