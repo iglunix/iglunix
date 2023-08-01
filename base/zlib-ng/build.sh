0b20790ec2c66753e8ef78e2a5edf033e6e096d8
Ella Stanforth <ella@iglunix.org>
2023-08-01 06:25:09 +0000

    zlib-ng: fix build

--- base/zlib-ng/build.sh
+++ base/zlib-ng/build.sh
@@ -1,5 +1,7 @@
 pkgname=zlib-ng
 pkgver=2.0.7
+mkdeps="cmake:samurai"
+deps="musl"
 
 fetch() {
 	curl -L "https://github.com/zlib-ng/zlib-ng/archive/refs/tags/$pkgver.tar.gz" -o $pkgname-$pkgver.tar.xz
