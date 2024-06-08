dd5109eac00571f2f6aab6a0cb40e55453b7f664
Ella Stanforth <ella@iglunix.org>
2024-06-08 01:34:55 +0000

    fix updatever.sh

--- scripts/updatever.sh
+++ scripts/updatever.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-NVER=$(curl "https://repology.org/project/$2/history" 2>/dev/null | grep "version-newest" | tr '>' ' ' | tr '<' ' ' | awk '{ print $5; }' | head -n1)
+REPO_VER=$(curl -A "Iglunix Package Updater" -L "https://repology.org/api/v1/project/$2" | jq '[.[] | select(.status=="newest")][0].version' | cut -d'"' -f2)
 TMP=$(mktemp)
-sed 's/pkgver=.*/pkgver='$NVER'/' $1/$2/build.sh > $TMP
+sed 's/pkgver=.*/pkgver='$REPO_VER'/' $1/$2/build.sh > $TMP
 mv $TMP $1/$2/build.sh
