1895d2a22dc6ad584fb45d4999953f34d855071d
Ella Stanforth <ella@iglunix.org>
2024-07-13 13:19:34 +0000

    libdrm: remove pciaccess

--- gui/libdrm/build.sh
+++ gui/libdrm/build.sh
@@ -1,4 +1,4 @@
-pkgver=2.4.118
+pkgver=2.4.122
 pkgname=libdrm
 # mkdeps="muon:samurai:pkgconf:python"
 # mkdeps="muon:pkgconf"
@@ -9,11 +9,15 @@ bad=""
 fetch() {
 	curl -L "https://dri.freedesktop.org/libdrm/libdrm-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.gz
 	tar -xf $pkgname-$pkgver.tar.gz
+	cd $pkgname-$pkgver
+	patch -p1 < ../../0001-intel-don-t-auto-disable-on-non-x86-platforms.patch
+	patch -p1 < ../../0002-intel-HACK-remove-pciaccess-dep.patch
 }
 
 build() {
 	cd $pkgname-$pkgver
 	muon setup $muon_base_args \
+		-Dtests=false \
 		build
 	samu -C build
 }
