73567c500bac071b7e80092853bde369f1832b86
Lucy Brown <19198652+anoraktrend@users.noreply.github.com>
2024-07-21 19:00:13 +0000

    fixed build.sh from hacky thing i did.
    Signed-off-by: Lucy Brown <19198652+anoraktrend@users.noreply.github.com>

--- extra/rust/build.sh
+++ extra/rust/build.sh
@@ -27,8 +27,10 @@ _clear_vendor_checksums() {
 if [ -d /opt/rust/bin ]
 then
 	export RUSTROOT="/opt/rust"
+elif { -f /root/.cargo }
+	export RUSTROOT="/root/.cargo"
 else
-	export RUSTROOT="/root/.cargo/"
+	export RUSTROOT="/usr"
 fi
 
 fetch() {
